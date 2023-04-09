3207e2b039c081c54c2272c6bcaaf61bc429780e
MumtahinaRahman <vanallope2015@gmail.com>
2023-04-09 11:53:10 +0600

    some assets added

---
+++ modern-bank-app/src/App.jsx
@@ -0,0 +1,12 @@
+import "./index.css"
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="bg-primary w-full overflow-hidden">
+      <h1>hello world!</h1>
+    </div>
+  )
+}
+
+export default App
